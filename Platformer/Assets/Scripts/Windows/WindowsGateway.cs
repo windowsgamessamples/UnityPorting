6c01a54311a4e468a4a5388fb29d4b93c0e1dc6c
Greg Quinn <greg.q@marker.co.nz>
2013-11-05 12:15:21 +1300

    Added WindowsGatewayInit scene and singleton instance script

--- Platformer/Assets/Scripts/Windows/WindowsGateway.cs
+++ Platformer/Assets/Scripts/Windows/WindowsGateway.cs
@@ -6,14 +6,42 @@ using System.Collections;
 /// <summary>
 /// Receives calls directly from Windows Store App
 /// </summary>
-public static class WindowsGateway  {
+public class WindowsGateway : MonoBehaviour
+{
+    static WindowsGateway _instance;
+    public static WindowsGateway Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(WindowsGateway)) as WindowsGateway;
+            }
+            return _instance;
+        }
+        set { _instance = value; }
+    }
+
+    public delegate void UnityLoadedHandler();
+    public static event UnityLoadedHandler UnityLoaded;
 
 	// called when window is resized
-	public static void WindowSizeChanged (double height, double width) {
+	public static void WindowSizeChanged (double height, double width) 
+    {
 	    // TODO deal with window resizing. e.g. if <= 500 implement pause screen
 	}
-	
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+        
+        // Code here to let the XAML app know that the first Unity scene has finished loading...
+        UnityLoaded();
+
+        // Now that our singleton WindowsGatewayObject exists, load the next game scene
+        Application.LoadLevel(1);
+    }
 }
 
 #endif
