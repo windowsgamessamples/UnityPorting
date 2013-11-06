280aba675a25c360c030a31545cf76674c37f3b8
Keith Patton <keith.p@markermetro.com>
2013-11-06 21:51:24 +1300

    made the plugins more efficient

--- PlatformerWindowsStore/MyPlugin/WindowsPlugin.cs
+++ PlatformerWindowsStore/MyPlugin/WindowsPlugin.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#if !UNITY_EDITOR
 using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Core;
+#endif
 
 namespace MyPlugin
 {
@@ -15,12 +17,24 @@ namespace MyPlugin
         /// <summary>
         /// Show the Share UI
         /// </summary>
+        
+#if UNITY_EDITOR
+
+        public static void ShowShareUI()
+        {
+
+        // do nothing
+
+#else
+
         public static async void ShowShareUI()
         {
+
             await CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.High, delegate()
             {
                 DataTransferManager.ShowShareUI();
             });
+#endif
         }
     }
 }
