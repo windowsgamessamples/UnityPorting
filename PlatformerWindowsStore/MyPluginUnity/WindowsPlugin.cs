b456a1d39b461efb846d3f2d5d0e69be46d9d2e5
Keith Patton <keith.p@markermetro.com>
2013-11-07 12:57:47 +1300

    fixing plugin support on WP8

---
+++ PlatformerWindowsStore/MyPluginUnity/WindowsPlugin.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+#if NETFX_CORE
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Core;
+#elif WINDOWS_PHONE
+using Microsoft.Phone.Tasks;
+#endif
+
+namespace MyPlugin
+{
+
+    public class WindowsPlugin
+    {
+
+        /// <summary>
+        /// Show the Share UI
+        /// </summary>
+        
+#if UNITY_EDITOR
+
+        public static void ShowShareUI()
+        {
+
+        // do nothing
+
+#elif NETFX_CORE
+
+        public static async void ShowShareUI()
+        {
+
+            await CoreWindow.GetForCurrentThread().Dispatcher.RunAsync(CoreDispatcherPriority.High, delegate()
+            {
+                DataTransferManager.ShowShareUI();
+            });
+
+#elif WINDOWS_PHONE
+
+        public static void ShowShareUI()
+        {
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+
+            shareLinkTask.Title = "Great Platformer Game!";
+            shareLinkTask.LinkUri = new Uri("http://code.msdn.com/wpapps", UriKind.Absolute);
+            shareLinkTask.Message = "Sharing the app for windows phone example!.";
+
+            shareLinkTask.Show();
+            
+#endif
+        }
+    }
+}
