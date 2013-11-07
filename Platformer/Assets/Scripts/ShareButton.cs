9ccc20e799bdaade0fd3c42c16b8b4a0a7a4229a
Keith Patton <keith.p@markermetro.com>
2013-11-07 22:09:33 +1300

    fix to ensure unity editor experience not broken for non windows platforms

--- Platformer/Assets/Scripts/ShareButton.cs
+++ Platformer/Assets/Scripts/ShareButton.cs
@@ -4,7 +4,9 @@ public class ShareButton : MonoBehaviour
 {
     void OnGUI()
     {
+#if UNITY_WINRT
         if (GUI.Button(new Rect(Screen.width - 120, 20, 100, 20), "Share"))
             WindowsGateway.ShowShareUI();
+#endif
     }
 }
