32e7c96715505dd649b5f00c6f59aac701bb89af
Greg Quinn <greg.q@marker.co.nz>
2013-11-07 14:41:57 +1300

    Added share button

---
+++ Platformer/Assets/Scripts/ShareButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ShareButton : MonoBehaviour 
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, 20, 100, 20), "Share"))
+            WindowsGateway.ShowShareUI();
+    }
+}
