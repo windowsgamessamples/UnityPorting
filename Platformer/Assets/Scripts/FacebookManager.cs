3baa8b39c7cdeeb90555a1cb201164193ab15c01
Greg Quinn <greg.q@marker.co.nz>
2013-11-15 10:42:20 +1300

    Added new FacebookManager and ShareManager scripts

---
+++ Platformer/Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles Facebook integration
+/// </summary>
+public class FacebookManager : MonoBehaviour
+{
+    private bool _loggedIn = false;
+    private const string FacebookAuthTokenKey = "FacebookAuthToken";
+
+    void OnGUI()
+    {
+
+        if (_loggedIn)
+        {
+            if (GUI.Button(new Rect(Screen.width - 150, 50, 130, 20), "Facebook logout"))
+            {
+                Logout();
+            }
+
+            if (GUI.Button(new Rect(Screen.width - 150, 80, 130, 20), "Invite friend"))
+            {
+                ShowFriends();
+            }
+        }
+        else
+        {
+            if (GUI.Button(new Rect(Screen.width - 150, 50, 130, 20), "Facebook login"))
+            {
+                Login();
+            }
+        }
+    }
+
+    private void ShowFriends()
+    {
+#if UNITY_WINRT
+        // TODO display a list of friends, and then when one is pressed call InviteFriend("friendid");
+        InviteFriend("TestUser");
+#endif
+    }
+
+    private void Login()
+    {
+#if UNITY_WINRT
+        MyPlugin.Facebook.FacebookPlugin.Login(result =>
+        {
+            if (!string.IsNullOrEmpty(result))
+            {
+                PlayerPrefs.SetString(FacebookAuthTokenKey, result);
+                PlayerPrefs.Save();
+            }
+        });
+#endif
+    }
+
+    private void Logout()
+    {
+#if UNITY_WINRT
+        MyPlugin.Facebook.FacebookPlugin.Logout(() =>
+        {
+            if (PlayerPrefs.HasKey(FacebookAuthTokenKey))
+            {
+                PlayerPrefs.DeleteKey(FacebookAuthTokenKey);
+                PlayerPrefs.Save();
+            }
+            _loggedIn = false;
+        });
+#endif
+    }
+
+    private void InviteFriend(string friend)
+    {
+#if UNITY_WINRT
+        MyPlugin.Facebook.FacebookPlugin.InviteFriend(friend);
+#endif
+    }
+
+}
---
+++ Platformer/Assets/Scripts/ShareManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles Share Integration
+/// </summary>
+public class ShareManager : MonoBehaviour 
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, 20, 100, 20), "Share"))
+        {
+#if UNITY_WINRT
+            MyPlugin.WindowsPlugin.ShowShareUI();
+#endif
+        }
+    }
+}
