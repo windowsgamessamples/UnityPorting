f803d27d16b2bb9ce5ee1928477c9f5a9d745a9e
Keith Patton <keith.p@markermetro.com>
2013-11-14 17:00:54 +1300

    removed the temp wack classes as integrating directly into the projects

--- WACKUtilsTemp/Store/StoreProduct.cs
+++
@@ -1,7 +0,0 @@
-public class StoreProduct
-{
-    public string ID { get; set; }
-    public string FriendlyName { get; set; }
-    public bool Owned { get; set; }
-    public string FormattedCost { get; set; }
-}
